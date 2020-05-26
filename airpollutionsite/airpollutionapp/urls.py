a9d1ba0a608ccf29718df714b8e008e813b9d428
heya <heyabird@gmail.com>
2020-05-25 17:38:12 -0700

    refractored and pseudocoded ursl.py

--- airpollutionsite/airpollutionapp/urls.py
+++ airpollutionsite/airpollutionapp/urls.py
@@ -3,22 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # lets try this again
-    # path('', views.get_air_quality_city, name='air quality'),
-    # path('bj/', views.get_air_quality_bj, name='BJ'),
+    # using one path for all database needed in this project
     path('<int:id>/', views.get_air_quality_city),
-    # path('average/<int:id>/', views.get_monthly_average),
-    # path('<int:id>/', views.detail, name='detail'),
-    # path('', views.index, name='index'),
-    # path('', views.receive_data_la, name='retrieveDataLA'),
-    # path('', views.receive_data_sd, name='retrieveDataSD'),
-    # path('', views.receive_data_ny, name='retrieveDataNY'),
-    # path('', views.receive_data_sf, name='retrieveDataSF'),
-    # path('', views.receive_data_nd, name='retrieveDataND'),
-    # path('', views.receive_data_bj, name='retrieveDataBJ'),
-    # path('', views.receive_data_ho, name='retrieveDataHO'),
-    # path('', views.receive_data_ch, name='retrieveDataCH'),
-
-
-
 ]
